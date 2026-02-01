e69956f39e80394a029dd08e2a1dea3afbaa788d
YusukeTanakaTomody <yusuke.tanaka@tomody.com>
2026-02-01 22:12:49 +0900

    fix: Fix config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default defineConfig({
-  plugins: [svelte()],
-  base: '/rust-primitives-visualizer/',
-})
+/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+}
